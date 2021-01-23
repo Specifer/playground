05279bcce7001f45e53e54ab5b90a19250d936c7
Specifer <64122067+Specifer@users.noreply.github.com>
2021-01-23 23:10:28 +0530

    Update death_match_manager.js

--- javascript/features/death_match/death_match_manager.js
+++ javascript/features/death_match/death_match_manager.js
@@ -213,12 +213,7 @@ export class DeathMatchManger {
         // Make sure this world is set last. TODO: Fix without wait. (e.g. use game API)
         wait(1).then(() => player.virtualWorld = location.world);
         player.interiorId = location.interiorId;
-
-        // Force-update the streamer for the player, based on where they will be spawning.
-        wait(0).then(() => player.updateStreamer(spawnPosition.position, location.world,
-        location.interiorId, /* STREAMER_TYPE_OBJECT= */ 0);
- 
-                     
+            
         // Do it in a tiny delay to avoid the player receiving an out of bounds message.
         wait(0).then(() => player.setWorldBoundaries(location.boundaries[0], location.boundaries[1],
             location.boundaries[2], location.boundaries[3]));
